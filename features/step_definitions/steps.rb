2f246e6ecd57fe69ea2436c821f475bfbbdec0d9
Alexander E. Fischer <aef@godobject.net>
2016-09-05 00:09:20 +0200

    Use temporary_directory gem

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -25,12 +25,15 @@ class ObjectExecutionResult < ExecutionResult
 end
 
 Before do
-  @temporary_directory = Pathname.new(Dir.mktmpdir('gem_metadata_cucumber'))
   @gem_dependencies = []
 end
 
+After do
+  ensure_absence_of_temporary_directory
+end
+
 Given(/^a dependency to a gem called "([^"]*)" with the following gemspec:$/) do |gem_name, gemspec_content|
-  gem_directory = @temporary_directory / gem_name
+  gem_directory = temporary_directory / gem_name
   gem_directory.mkdir
 
   gemspec_file = gem_directory / "#{gem_name}.gemspec"
@@ -45,7 +48,7 @@ When(/^the following code is executed:$/) do |code_to_execute|
                              .parent
                              .parent
 
-  gemfile = @temporary_directory / 'Gemfile'
+  gemfile = temporary_directory / 'Gemfile'
   gemfile.open('w') do |io|
     io.puts %{gem 'gem_metadata', path: '#{project_root_directory}'} 
 
@@ -54,7 +57,7 @@ When(/^the following code is executed:$/) do |code_to_execute|
     end
   end
 
-  code_file = @temporary_directory / 'code.rb'
+  code_file = temporary_directory / 'code.rb'
   code_file.write <<~CODE
     def code_to_execute  
       #{code_to_execute}
@@ -62,7 +65,7 @@ When(/^the following code is executed:$/) do |code_to_execute|
   CODE
 
   Bundler.with_clean_env do
-    Dir.chdir(@temporary_directory) do
+    Dir.chdir(temporary_directory) do
       @execution_result = ObjectExecutionResult.new(
         console_output: `bundle exec ruby -r'#{code_file}' -e 'puts Marshal.dump(code_to_execute)'`
       )
@@ -89,8 +92,4 @@ Then(/^the result lists the gems named "([^"]*)" and "([^"]*)"$/) do |first_gem_
   expect(object_result[0].name).to eql first_gem_name
   expect(object_result[1]).to be_a Gem::Specification
   expect(object_result[1].name).to eql second_gem_name
-end
-
-After do
-  @temporary_directory.rmtree
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,1 +1,4 @@
 require 'rspec/collection_matchers'
+require 'temporary_directory'
+
+World(GodObject::TemporaryDirectory::Helper.new(name_prefix: 'gem_metadata_cucumber'))
